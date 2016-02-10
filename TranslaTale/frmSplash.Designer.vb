9489586d1a4628c52a3791488f158d8dac7726c3
Alex Wavediver <avixthefox@gmail.com>
2016-02-10 23:29:18 +0100

    Argh, some minor fixes (v0.8 instead of v8.0!)

--- TranslaTale/frmSplash.Designer.vb
+++ TranslaTale/frmSplash.Designer.vb
@@ -23,6 +23,7 @@ Partial Class frmSplash
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmSplash))
         Me.tmrSplash = New System.Windows.Forms.Timer(Me.components)
         Me.SuspendLayout()
         '
@@ -38,6 +39,7 @@ Partial Class frmSplash
         Me.BackgroundImage = Global.TranslaTale.My.Resources.Resources.splash
         Me.ClientSize = New System.Drawing.Size(356, 264)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "frmSplash"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "frmSplash"
